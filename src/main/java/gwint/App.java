f182f8ae04960daf6052cbd3f36a95d50821a4fc
Jakub Binięda <jakub.binieda@icloud.com>
2022-03-28 21:28:48 +0200

    Slight changes + gitignore

--- src/main/java/gwint/App.java
+++ src/main/java/gwint/App.java
@@ -1,10 +1,13 @@
+/*
+ * PO Projekt - Karcianka chwilowo bez nazwy
+ * @author Hurkala Binieda Sanocki
+ * 
+ */
+
 package gwint;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 import javafx.stage.Screen;
@@ -12,22 +15,20 @@ import javafx.geometry.Rectangle2D;
 
 import java.io.IOException;
 
-/**
- * JavaFX App
- */
 public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
+        //Get screen dimentions
         Rectangle2D screenBounds = Screen.getPrimary().getBounds();
         double height=screenBounds.getHeight();
         double width=screenBounds.getWidth();
 
+        //Set stage
         stage.setTitle("Base Game");
         stage.setFullScreen(true);
         GridPane root = new GridPane();
         String image = App.class.getResource("plansza.png").toExternalForm();
-        //Image Plan = new Image(image);
         root.setStyle(
             "-fx-background-image: url('" + image + "'); " +
             "-fx-background-position: right center;" + 
@@ -37,11 +38,13 @@ public class App extends Application {
         );
         stage.setResizable(false);
         
+        //Add scene to stage and show it
         stage.setScene(new Scene(root,width,height));
         stage.show();
     }
     
     public static void main(String[] args) {
+        //3 2 1 and launch 🚀
         launch(args);
         
     }
