6dfc4f4433804ff51fa6ee7989dc90e2a5869828
Jakub Binięda <jakub.binieda@icloud.com>
2022-03-28 20:29:04 +0200

    Changed app dimentions

--- src/main/java/gwint/App.java
+++ src/main/java/gwint/App.java
@@ -7,6 +7,8 @@ import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+import javafx.stage.Screen;
+import javafx.geometry.Rectangle2D;
 
 import java.io.IOException;
 
@@ -17,18 +19,25 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
+        Rectangle2D screenBounds = Screen.getPrimary().getBounds();
+        double height=screenBounds.getHeight();
+        double width=screenBounds.getWidth();
+
         stage.setTitle("Base Game");
+        stage.setFullScreen(true);
         GridPane root = new GridPane();
-        String image = App.class.getResource("plansza.jpg").toExternalForm();
+        String image = App.class.getResource("plansza.png").toExternalForm();
         //Image Plan = new Image(image);
         root.setStyle(
             "-fx-background-image: url('" + image + "'); " +
-            "-fx-background-position: center top;" + 
+            "-fx-background-position: right center;" + 
             "-fx-background-repeat: no-repeat; " +
-            "-fx-background-size: 1280 560; "
+            "-fx-background-size: cover" 
+            //MOZNA ZMIENIC NA CONTAIN 
         );
         stage.setResizable(false);
-        stage.setScene(new Scene(root,1280,760));
+        
+        stage.setScene(new Scene(root,width,height));
         stage.show();
     }
     
