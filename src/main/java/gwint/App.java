8140236f98bd4976c69e2e46b6ec247ea95af84a
mattti <mattti2015@gmail.com>
2022-03-28 16:45:31 +0200

    random_backgorund

--- src/main/java/gwint/App.java
+++ src/main/java/gwint/App.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
@@ -18,9 +19,16 @@ public class App extends Application {
     public void start(Stage stage) throws IOException {
         stage.setTitle("cos");
         GridPane root = new GridPane();
-        root.setStyle("-fx-background-color: brown");
+        String image = App.class.getResource("plansza.jpg").toExternalForm();
+        Image Plan = new Image(image);
+        root.setStyle(
+            "-fx-background-image: url('" + image + "'); " +
+           "-fx-background-position: center top;" + 
+           "-fx-background-repeat: no-repeat; " +
+            "-fx-background-size: 1280 560; "
+        );
         stage.setResizable(false);
-        stage.setScene(new Scene(root,640,480));
+        stage.setScene(new Scene(root,1280,760));
         stage.show();
     }
 
