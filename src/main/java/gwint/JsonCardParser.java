8923e65b63cc5a8bb71b9033a2662e5f4f665ef1
prolik123 <mattti2015@gmail.com>
2022-03-29 15:40:04 +0200

    JsonCardParser.java Changes

--- src/main/java/gwint/JsonCardParser.java
+++ src/main/java/gwint/JsonCardParser.java
@@ -5,9 +5,26 @@ import java.io.FileReader;
 import java.util.*;
 import org.json.*;
 
+    /// Get a random permutation of the list
+    /// Collections.shuffle(result);
+
 public class JsonCardParser {
     
-    public static List<Card> getCardList() throws JSONException, FileNotFoundException{
+    public static List<Card> getCardsList() {
+        try{
+            return getCardListWithExeptions();
+        }
+        catch(JSONException e){
+            /// failure
+            throw new RuntimeException("You got JSONExeption from JsonParser");
+        }
+        catch(FileNotFoundException e) {
+            /// failure
+            throw new RuntimeException("You got FileNotFoundException from JsonParser");
+        }
+    }
+
+    public static List<Card> getCardListWithExeptions() throws JSONException, FileNotFoundException{
 
         List<Card> result = new ArrayList<>();
 
@@ -21,27 +38,33 @@ public class JsonCardParser {
             Card currentCard = new Card();
             /// Getting k-th Json value form the array
             JSONObject currentJsonValue = (JSONObject) arr.get(k);
+            JSONObject currentJsonCard = (JSONObject) currentJsonValue.get("card");
 
             /// gettting all of aribiutes
-            currentCard.name = (String) (((JSONObject)
-                currentJsonValue.get("card")).get("name"));
+            currentCard.name = getJsonStringAttribute(currentJsonCard,"name");
                 
-            currentCard.imageLink = (String) (((JSONObject)
-                currentJsonValue.get("card")).get("link"));
+            currentCard.imageLink = getJsonStringAttribute(currentJsonCard, "link");
 
-            currentCard.value = Integer.parseInt(((String) 
-                (((JSONObject)currentJsonValue.get("card")).get("value"))));
+            currentCard.value = getJsonIntegerAttribute(currentJsonCard, "value");
 
-            int quantity = Integer.parseInt((String) 
-                (((JSONObject)currentJsonValue.get("card")).get("quantity")));
+            int quantity = getJsonIntegerAttribute(currentJsonCard, "quantity");
             
             /// add qunatity times k-th Card to List
             for(int j=0;j<quantity;j++)
                 result.add(currentCard);
         }
 
-        /// Get a random permutation of the list
-        Collections.shuffle(result);
         return result;
     }
+
+    /// You must be sure that the attribute is String
+    public static String getJsonStringAttribute(JSONObject object,String attributeName) {
+        return (String) object.get(attributeName);
+    }
+
+    /// You must be sure that the attribute is Integer
+    public static Integer getJsonIntegerAttribute(JSONObject object,String attributeName) {
+        return Integer.parseInt((String) object.get(attributeName));
+    }
+
 }
