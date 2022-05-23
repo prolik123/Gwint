7a0999a374720b805c2795b9f6d7058874cc5266
Maciej Sanocki <msanock@gmail.com>
2022-05-23 19:42:16 +0200

    completed Settings

--- src/main/java/gwint/Sounds.java
+++ src/main/java/gwint/Sounds.java
@@ -9,6 +9,8 @@ public class Sounds {
 
     public static MediaPlayer soundEffectPlayer;
 
+    public static double soundEffectVolume;
+
     public static void playBackgroundMusic() {
         Media backgroundMusic=new Media(new File(Constants.backgroundSoundPath+"background.mp3").toURI().toString());
         backgroundMusicPlayer=new MediaPlayer(backgroundMusic);
@@ -18,6 +20,7 @@ public class Sounds {
     public static void playSoundEffect(String name) {
         Media soundEffect=new Media(new File(Constants.backgroundSoundPath+name+".mp3").toURI().toString());
         soundEffectPlayer=new MediaPlayer(soundEffect);
+        soundEffectPlayer.setVolume(soundEffectVolume);
         soundEffectPlayer.play();
     }
 }
